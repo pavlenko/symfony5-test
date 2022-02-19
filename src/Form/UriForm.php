9bd7c4b8b115a05cdee7c2bb8d3824e7b438b249
Pavlenko Eugene <pavlenko.obs@gmail.com>
2022-02-19 08:53:54 +0200

    add bootstrap styles

--- src/Form/UriForm.php
+++ src/Form/UriForm.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +18,7 @@ class UriForm extends AbstractType
             ->add('uri', UrlType::class)
             ->add('maxRedirects', IntegerType::class)
             ->add('expiredAt', DateTimeType::class)
+            ->add('generate', SubmitType::class)
         ;
     }
 
